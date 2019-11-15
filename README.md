1a21545224d8bcfb2d3a305fee6c1667441c77a5
Omar Aguinaga <omar@unumotors.com>
2019-11-15 11:23:30 +0100

    unu animation workshop initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🤹🏽‍♂️Animation Workshop
+
+## Initial setup
+
+1. Clone repository
+2. Go to clone directory
+3. Install dependencies  `npm i`
+4. Run project `npm run start`
+5. Visit  `http://localhost:1234` or the one shown in the terminal as `Server running at http://localhost:xxxx`
---
+++ index.js
@@ -0,0 +1,16 @@
+import 'normalize.css';
+
+const sheet = document.querySelector('.sheet');
+const closedHeigh = 400;
+const openHeight = 800;
+let isOpen = false;
+
+sheet.addEventListener('click', (e) => {
+});
+
+sheet.addEventListener('touchstart', (e) => {
+});
+
+
+sheet.addEventListener('touchmove', (e) => {
+});
